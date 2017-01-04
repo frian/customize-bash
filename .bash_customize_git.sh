6f37c460845115eaac27614d80223967715c67f9
frian <a@frian.org>
2017-01-04 18:55:26 +0100

    added gitp alias

--- .bash_customize_git.sh
+++ .bash_customize_git.sh
@@ -3,6 +3,7 @@ alias gitc=gitCommit
 alias gitd='git diff'
 alias giti='git init'
 alias gitl='git log --decorate --all --oneline --graph'
+alias gitp='git push'
 alias gits='git status'
 
 gitCommit() {
