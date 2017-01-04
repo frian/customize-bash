b62273f9366947d6657409e9ccbf45b09b824713
frian <a@frian.org>
2017-01-04 18:43:22 +0100

    changed script name detection

--- customize-shell.sh
+++ customize-shell.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
+#
+# -- get and cd in script dir
+#
+
+# -- Absolute path to this script
+SCRIPT=${BASH_SOURCE[0]}
+
+# -- Absolute path to script dir
+SCRIPTPATH=$(dirname "${SCRIPT}")
+
+# -- cd to script dir for config sourcing
+cd $SCRIPTPATH
+
 # -- copy files to $HOME remove file extension
 echo "  copy files to \$HOME"
 for file in .*.sh; do
