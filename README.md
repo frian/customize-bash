171cda8e6ce1d9a892e566587abbd1fe29be4b6f
frian <a@frian.org>
2017-01-04 22:28:01 +0100

    more comments

---
+++ README.md
@@ -0,0 +1,21 @@
+customize bash
+==============
+
+configurable and extendable bash customisation
+
+- colored prompt
+- colored man pages
+
+files
+-----
+
+| File | Description |
+| --- | --- |
+| customize-bash.sh | the installer script |
+| .bash_customize.sh | main file, loads all other files |
+| .bash_customize_shell.sh | common shell aliases and env variables |
+| .bash_customize_prompt_colors.sh | colored prompt |
+| .bash_customize_ssh.sh | ssh aliases and env variables |
+| .bash_customize_git.sh | git aliases and env |
+| .bash_customize_symfony.sh | symfony aliases and env |
+| .bash_customize_compass.sh | compass aliases and env |
--- customize-bash.sh
+++ customize-bash.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+
+
 #
 # -- get and cd in script dir
 #
@@ -13,15 +15,22 @@ SCRIPTPATH=$(dirname "${SCRIPT}")
 # -- cd to script dir for config sourcing
 cd $SCRIPTPATH
 
+#
 # -- copy files to $HOME remove file extension
+#
 echo "  copy files to \$HOME"
 for file in .*.sh; do
     cp "$file" "$HOME/`basename "$file" .sh`"
 done
 
+
 echo "  cd to \$HOME"
 cd $HOME
 
+
+#
+# -- add . ~/.bash_customize to .bshrc
+#
 echo "  add .bash_customize sourcing to bash if not present"
 if ! grep -q ". ~/.bash_customize" .bashrc; then
     # -- add source the file to bashrc
@@ -29,6 +38,7 @@ if ! grep -q ". ~/.bash_customize" .bashrc; then
     echo "# -- shell customization" >> .bashrc
     echo ". ~/.bash_customize" >> .bashrc
 fi
+
 
 # -- load bashrc
 echo "  reloading config"
