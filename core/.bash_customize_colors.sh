4d0ac1f183ec309a4f8aa41d79d0ea413e55a1b8
frian <a@frian.org>
2017-01-06 16:57:30 +0100

    changed color codes for awk

--- core/.bash_customize_colors.sh
+++ core/.bash_customize_colors.sh
@@ -3,46 +3,46 @@
 #
 
 # Normal Colors
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Purple='\e[0;35m'       # Purple
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
 
 # Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BPurple='\e[1;35m'      # Purple
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
 
 # Background
-On_Black='\e[40m'       # Black
-On_Red='\e[41m'         # Red
-On_Green='\e[42m'       # Green
-On_Yellow='\e[43m'      # Yellow
-On_Blue='\e[44m'        # Blue
-On_Purple='\e[45m'      # Purple
-On_Cyan='\e[46m'        # Cyan
-On_White='\e[47m'       # White
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
 
-NC="\e[m"               # Color Reset
+NC="\033[m"               # Color Reset
 
 
 #
 # -- less colors --------------------------------------------------------------
 #
-LESS_BLACK=$'\E[01;30m'
-LESS_RED=$'\E[01;31m'
-LESS_GREEN=$'\E[01;32m'
-LESS_YELLOW=$'\E[01;33m'
-LESS_BLUE=$'\E[01;34m'
-LESS_PURPLE=$'\E[01;35m'
-LESS_CYAN=$'\E[01;36m'
-LESS_WHITE=$'\E[01;37m'
+LESS_BLACK=$'\033[01;30m'
+LESS_RED=$'\033[01;31m'
+LESS_GREEN=$'\033[01;32m'
+LESS_YELLOW=$'\033[01;33m'
+LESS_BLUE=$'\033[01;34m'
+LESS_PURPLE=$'\033[01;35m'
+LESS_CYAN=$'\033[01;36m'
+LESS_WHITE=$'\033[01;37m'
