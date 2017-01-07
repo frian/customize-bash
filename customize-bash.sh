502d6d8293fead705e085e10ff1dbb6e68f3fe5e
frian <a@frian.org>
2017-01-07 16:16:17 +0100

    re-added generate bash_customize.sh

--- customize-bash.sh
+++ customize-bash.sh
@@ -10,6 +10,9 @@ SCRIPT=${BASH_SOURCE[0]}
 # -- Absolute path to script dir
 SCRIPTPATH=$(dirname "${SCRIPT}")
 
+# -- default profile
+PROFILE=default
+
 # -- cd to script dir for config sourcing
 cd $SCRIPTPATH
 
@@ -26,36 +29,41 @@ else
     rm -rf $INSTALLPATH/.* $INSTALLPATH/* 2>/dev/null
 fi
 
-# #
-# # -- generate core/.bash_customize.sh
-# #
-# CONFIG_FILE=core/.bash_customize.sh
-# CONFIG_TMP_FILE=core/bash_customize.sh
 #
-# if [[ -e $CONFIG_FILE ]]; then
-#     rm $CONFIG_FILE
-# fi
+# -- generate core/.bash_customize.sh
 #
-# # -- add install path
-# echo INSTALLPATH=$HOME/.customize-bash >> $CONFIG_TMP_FILE
-#
-# # -- add core files
-# for file in core/.*.sh; do
-#     echo . "$INSTALLPATH/`basename "$file" .sh`" >> $CONFIG_TMP_FILE
-# done
-#
-# # -- add personal files
-# for file in personal/.*.sh; do
-#     echo . "$INSTALLPATH/`basename "$file" .sh`" >> $CONFIG_TMP_FILE
-# done
-#
-#
-# # -- remove bogus line if personal is empty
-# sed -i '/.*\.\*$/d' $CONFIG_TMP_FILE 1>/dev/null
-#
-#
-# mv $CONFIG_TMP_FILE $CONFIG_FILE
+CONFIG_FILE=profiles/$PROFILE/bash_customize.sh
+CONFIG_TMP_FILE=profiles/$PROFILE/bash_customize.sh.tmp
 
+
+
+if [[ -e $CONFIG_FILE ]]; then
+    rm $CONFIG_FILE
+fi
+
+# -- add install path
+echo INSTALLPATH=$HOME/.customize-bash >> $CONFIG_TMP_FILE
+
+# -- add core files
+for file in core/*.sh; do
+    echo $file
+    echo . "$INSTALLPATH/.`basename "$file" .sh`" >> $CONFIG_TMP_FILE
+done
+
+# -- add personal files
+for file in profiles/$PROFILE/*.sh; do
+    echo $file
+    echo . "$INSTALLPATH/.`basename "$file" .sh`" >> $CONFIG_TMP_FILE
+done
+
+
+# -- remove bogus line if personal is empty
+sed -i '/.*\.\*$/d' $CONFIG_TMP_FILE 1>/dev/null
+
+
+mv $CONFIG_TMP_FILE $CONFIG_FILE
+
+# exit
 
 #
 # -- copy core files to $HOME/.customize-bash remove file extension
@@ -70,7 +78,7 @@ done
 # -- copy personal files to $HOME/.customize-bash remove file extension
 #
 echo "  copy personal files to \$HOME"
-for file in profiles/default/*.sh; do
+for file in profiles/$PROFILE/*.sh; do
     cp "$file" "$INSTALLPATH/.`basename "$file" .sh`" 2>/dev/null
 done
 
